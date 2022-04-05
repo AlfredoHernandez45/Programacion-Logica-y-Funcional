005a229f9ec210b72a1d9b48964217e146ef84a1
AlfredoHernandez45 <90002912+AlfredoHernandez45@users.noreply.github.com>
2022-04-05 14:49:49 -0500

    update

--- 10_Numeros_Primos.hs
+++ 10_Numeros_Primos.hs
@@ -9,3 +9,10 @@ esPrimo n = factores n == [1,n]
 -- Filtrar los números naturales
 primos :: [Int] -> [Int]
 primos n = [x | x <- [2..n], esPrimo x]
+
+--primos :: [Int]
+--primos = criba [2..] --lista infinita de números naturales
+
+--Implementamos la criba recursivamente
+--criba :: [Int] -> [Int]
+--criba (p:xs) = p : criba [x | x <- xs, x `mod` p /= 0]
