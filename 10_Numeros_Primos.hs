30aa10a5f2a982fb3720ae87aad60931eafff6d9
AlfredoHernandez45 <90002912+AlfredoHernandez45@users.noreply.github.com>
2022-04-05 17:20:17 -0500

    correcion del programa
    se limito hasta 100 numeros primos

--- 10_Numeros_Primos.hs
+++ 10_Numeros_Primos.hs
@@ -1,18 +1,14 @@
--- Calcular los factores 
-factores :: Int -> [Int]
-factores n = [x | x <- [1..n], n `mod` x == 0]
+-- --------------------------------------------------------------------------
+-- Ejercicio 10.
+-----------------------------------------------------------------------------
+-- Define las función "criba" la cual recibe un "Int" y regresa un "Int" e 
+-- implementa recursividad para sacar los numeros primos, la función "primos"
+-- llama a la funcion criba a la cual se le asigna un rango de 2 a 100 para 
+-- imprimir numeros primos
 
--- Será primo solo si sus factores son 1 y él mismo
-esPrimo :: Int -> Bool
-esPrimo n = factores n == [1,n]
-
--- Filtrar los números naturales
-primos :: [Int] -> [Int]
-primos n = [x | x <- [2..n], esPrimo x]
-
---primos :: [Int]
---primos = criba [2..] --lista infinita de números naturales
+primos :: [Int]
+primos = criba [2 .. 100] --lista infinita de números naturales
 
 --Implementamos la criba recursivamente
---criba :: [Int] -> [Int]
---criba (p:xs) = p : criba [x | x <- xs, x `mod` p /= 0]
+criba :: [Int] -> [Int]
+criba (p:xs) = p : criba [x | x <- xs, x `mod` p /= 0]
